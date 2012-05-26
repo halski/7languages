52e4cc9758a7372fbd4428738f962bafbca5ca7e
Kuba Michalski <halski@gmail.com>
2012-05-27 00:58:51 +0200

    Day 1 Ruby commit

---
+++ ruby/day-1-all.rb
@@ -0,0 +1,14 @@
+###
+puts 'Hello, Welt !'
+
+###
+puts 'Hello, Ruby!'
+index ='Hello, Ruby!'.index('Ruby')
+puts "Index of \'Ruby\': #{index}"
+
+###
+x=0
+while x<10
+	puts "Kuba : #{x}"
+	x+=1
+end
